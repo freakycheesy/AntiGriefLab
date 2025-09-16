aae7cf7c3790e3f66f5c21bed5f3f9c781548af6
basicprune <stephenbluett2@gmail.com>
2025-09-16 23:22:53 +1000

    InitialCommit

---
+++ BonelabFusionAntiGrief/AntiGrief.cs
@@ -0,0 +1,57 @@
+﻿using HarmonyLib;
+using LabFusion;
+using LabFusion.Entities;
+using LabFusion.Network;
+using LabFusion.Network.Serialization;
+using LabFusion.Player;
+using MelonLoader;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+using UnityEngine;
+using static Il2Cpp.Interop;
+
+
+
+
+
+namespace BonelabFusionAntiGrief 
+{
+    [HarmonyPatch(typeof(RigNameTag))]
+    public class AntiGrief : MelonMod
+    {
+       
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+            HarmonyInstance.PatchAll();
+        }
+
+        [HarmonyPatch(typeof(ConnectionRequestMessage))]
+        public static class ConnectionRequestMessagePatch
+        {
+            [HarmonyPatch("OnHandleMessage")]
+            [HarmonyPrefix]
+            public static void Prefix(ReceivedMessage received)
+            {
+                try
+                {
+                    var data = received.ReadData<ConnectionRequestData>();
+                    MelonLogger.Msg($"[AntiGrief] Incoming connection attempt: PlatformID={data.PlatformID}, Version={data.Version}");
+
+                }
+                catch (Exception ex)
+                {
+                    MelonLogger.Warning($"[AntiGrief] Failed to log connection attempt: {ex}");
+                }
+            }
+        }
+
+
+    }
+
+
+}
